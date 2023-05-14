cabab464571e8f7d1e7f3afe171ea0d56904de9a
fahrezyman <ilhamfahrezyxz@gmail.com>
2023-05-14 09:13:09 +0700

    Project Files

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class Spawner : MonoBehaviour{
+    public GameObject prefab;
+    public float spawnRate = 0.2f;
+    public float minHeight = -0.2f;
+    public float maxHeight = 0.2f;
+
+    private void OnEnable(){
+        InvokeRepeating(nameof(Spawn),spawnRate,spawnRate);
+    }
+
+    private void OnDisable(){
+        CancelInvoke(nameof(Spawn));
+    }
+
+    private void Spawn(){
+        GameObject pipes = Instantiate(prefab, transform.position, Quaternion.identity);
+        pipes.transform.position += Vector3.up * Random.Range(minHeight,maxHeight); 
+        Debug.Log("Spawning object...");
+    }
+}
